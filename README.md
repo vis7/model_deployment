4df7cfdbcef3e02c0da4c7ba002a78b2691f5e6c
vis7 <vishvajeetramanuj95@gmail.com>
2021-11-08 10:47:08 +0530

    initialized repository and pytorch inference

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # model_deployment
 model deployment tutorial code
+
+# Introductory description of content of repository
+- pytorch_model_deployment contain code for deploying pytoch model with flask app
+
---
+++ pytorch_model_deployment/inference.py
@@ -0,0 +1,54 @@
+import io
+
+import torchvision.transforms as transforms
+from PIL import Image
+
+# transforming image
+def transform_image(image_bytes):
+    my_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image).unsqueeze(0)
+
+# # tesing the transformation of image
+# with open("pop_ww.jpg", 'rb') as f:
+#     image_bytes = f.read()
+#     tensor = transform_image(image_bytes=image_bytes)
+#     print(tensor)
+
+# using pretrained model to make prediction
+from torchvision import models
+
+# loading densenet 121 model
+model = models.densenet121(pretrained=True)
+# Since we are using our model only for inference, switch to `eval` mode:
+model.eval()
+
+# making prediction
+def get_prediction(image_bytes):
+    tensor = transform_image(image_bytes=image_bytes)
+    outputs = model.forward(tensor)
+    _, y_hat = outputs.max(1)
+    return y_hat
+
+
+import json
+
+imagenet_class_index = json.load(open('static/imagenet_class_index.json'))
+
+def get_prediction(image_bytes):
+    tensor = transform_image(image_bytes=image_bytes)
+    outputs = model.forward(tensor)
+    _, y_hat = outputs.max(1)
+    predicted_idx = str(y_hat.item())
+    return imagenet_class_index[predicted_idx]
+
+# testing image_net_class file
+with open("static/img/pop_ww.jpg", 'rb') as f:
+    image_bytes = f.read()
+    print(get_prediction(image_bytes=image_bytes))
+
